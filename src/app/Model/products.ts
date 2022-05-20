adec0783f5c65092b8cfaa520466d471825c2852
Akram_Kadri <akram.kadri@newaccess.ch>
2022-05-20 11:42:31 +0100

    Initial commit

---
+++ src/app/Model/products.ts
@@ -0,0 +1,9 @@
+export interface Products {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    quantity: number;
+    imageUrl: string;
+    
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule,} from '@angular/material/toolbar';
+import {MatIconModule } from '@angular/material/icon';
+import {MatButtonModule } from '@angular/material/button';
+import {MatCardModule } from '@angular/material/card';
+import {MatProgressSpinnerModule } from '@angular/material/progress-spinner'; 
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
